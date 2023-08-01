49b626ad6388283564120e1700b07cd73e1de594
hmgene <human.gim@gmail.com>
2023-08-01 15:40:38 -0400

    hi

--- app/app.py
+++ app/app.py
@@ -4,8 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world(name=None):
+def root(name=None):
     return render_template('sankey.html', name=name)
+
+@app.route("/data")
+def data(name=None):
+    return 'data'
 
 if __name__ == "__main__":
 	app.run(debug=True)
